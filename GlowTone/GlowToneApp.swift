4f4dbd3e99ec2665fded7ae1e1aa3eb155804a01
Kiara Mathews <kiara.n.mathews@gmail.com>
2023-06-13 10:15:41 -0500

    Initial Commit

---
+++ GlowTone/GlowToneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlowToneApp.swift
+//  GlowTone
+//
+//  Created by scholar on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct GlowToneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
